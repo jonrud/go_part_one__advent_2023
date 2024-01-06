ae6e6ab4dae9d3f57a27e82dc4cf9b61ee30d71a
Jonas Rudžionis <rudzionisjonas@gmail.com>
2024-01-06 21:40:44 +0200

    Initial commit

---
+++ day_one/run.go
@@ -0,0 +1,5 @@
+package day_one
+
+func Run() string {
+	return "In progress..."
+}
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"advent_2023/day_one"
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Day one result: ", day_one.Run())
+}
