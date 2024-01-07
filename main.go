b38989171f49e58620d8daac99001d77f6633831
Jonas Rudžionis <rudzionisjonas@gmail.com>
2024-01-07 19:36:18 +0200

    D2P2 done (missed files)

--- main.go
+++ main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
-	"advent_2023/day_one"
+	"advent_2023/day_two"
 	"fmt"
 )
 
 func main() {
-	fmt.Println("Day one part one result: ", day_one.RunPartOne())
-	fmt.Println("Day one part two result: ", day_one.RunPartTwo())
+	//fmt.Println("Day one part one result: ", day_one.RunPartOne())
+	//fmt.Println("Day one part two result: ", day_one.RunPartTwo())
+	//fmt.Println("Day two part two result: ", day_two.RunPartOne())
+	fmt.Println("Day two part two result: ", day_two.RunPartTwo())
 }
